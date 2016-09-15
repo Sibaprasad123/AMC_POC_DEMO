ff37c5e706123cc7ca1b422218f3ae47b6c135a0
sibaprasad <nayaksiba1991@gmail.com>
2016-09-15 15:28:22 +0530

    Signed-off-by: sibaprasad <nayaksiba1991@gmail.com>

--- TestNG.bat
+++ TestNG.bat
@@ -1,2 +1,2 @@
     cd C:\workspace\AMC_POC_DEMO
-    java -cp C:\workspace\AMC_POC_DEMO\lib*;C:\workspace\AMC_POC_DEMO\bin org.testng.TestNG testng.xml
+    java -cp C:\workspace\AMC_POC_DEMO\bin;C:\workspace\AMC_POC_DEMO\lib* org.testng.TestNG testng.xml
