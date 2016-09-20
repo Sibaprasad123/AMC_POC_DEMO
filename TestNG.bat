88f802837f7afbf10bc89b4b19cee89d400aa931
sibaprasad <nayaksiba1991@gmail.com>
2016-09-20 17:48:20 +0530

    Signed-off-by: sibaprasad <nayaksiba1991@gmail.com>

--- TestNG.bat
+++ TestNG.bat
@@ -1,4 +1,4 @@
-set projectLocation=C:\workspace\AMC_POC_DEMO
+set projectLocation=.\..\AMC_POC_DEMO
 cd %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\lib\*
 java org.testng.TestNG %projectLocation%\testng.xml
